e6fdba974fbca2066fee96e52b7830e0ea61d856
ahmedelqori <aelqori1@gmail.com>
2023-06-26 22:39:09 +0100

    Add printf

---
+++ _printf.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ *_printf - print string with argument
+ *@format: string and specfier
+ *Return: number of specfier
+ */
+
+int _printf(const char *format, ...)
+{
+	return (0);
+}
+
